928514ee8d3fb362c938ee4a18ed4d4fe3adefe3
avidan.abitbol <avidan88@walla.com>
2024-07-25 17:50:37 +0300

    Update Node.h

--- sources/Node.h
+++ sources/Node.h
@@ -4,63 +4,44 @@
 #include <memory>
 #include <vector>
 
+/**
+ * @brief This class represents a node in a tree data structure.
+ *
+ * @tparam T The data type of the elements stored in the tree nodes.
+ */
 template<typename T>
 class Node {
-
 public:
-    T data;
-    int numOfChildren = 0;
+    T data; ///< The data stored in this node.
+    int numOfChildren = 0; ///< The current number of children this node has.
 
-    std::vector<std::shared_ptr<Node<T>>> children;
+    std::vector<std::shared_ptr<Node<T>>> children; ///< Dynamic array of shared pointers to the node's children.
 
+    /**
+     * @brief Construct a new Node object with the given data.
+     *
+     * @param value The data value to store in this node.
+     */
     explicit Node(T value) : data(value) {}
 
+    /**
+     * @brief Resizes the children vector to a specified size and initializes them to nullptr.
+     *
+     * @param numChildren The new size of the children vector.
+     */
     void resize_children(int numChildren) {
         children.resize(static_cast<size_t>(numChildren), nullptr);
     };
 
+    /**
+     * @brief Sets the data of this node.
+     *
+     * @param value The new data value for this node.
+     */
     void set_data(T value) {
         data = value;
     }
 };
 
 #endif // NODE_HPP
-
-
-
-//#ifndef NODE_HPP
-//#define NODE_HPP
-//
-//#include <vector>
-//#include <cstddef>
-//
-//template<typename T>
-//class Node {
-//public:
-//    int numOfChildren = 0;
-//    T data;
-//    std::vector<Node<T> *> children;
-//
-//    explicit Node(T value) : data(value) {}
-//
-//    void add_child(Node<T> *child) {
-//        children.push_back(child);
-//    }
-//
-//    void resize_children(int numChildren) {
-//        children.resize(static_cast<size_t>(numChildren), nullptr);
-//    }
-//
-//    void set_data(T value) {
-//        data = value;
-//    }
-//
-//    ~Node() {
-//        for (auto child: children) {
-//            delete child;  // Delete each child node
-//        }
-//    }
-//};
-//
-//#endif // NODE_HPP
 
